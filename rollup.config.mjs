59e46079e2475c92409c00a43857a4e99ebba276
vladgitx <vladd.balea@gmail.com>
2024-04-22 15:45:40 +0300

    Add json plugin to rollup

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,6 +2,7 @@ import del from "rollup-plugin-delete"
 import typescript from "@rollup/plugin-typescript"
 import resolve from "@rollup/plugin-node-resolve"
 import commonjs from "@rollup/plugin-commonjs"
+import json from "@rollup/plugin-json"
 
 export default {
 	input: "./src/index.ts",
@@ -16,5 +17,6 @@ export default {
 		}),
 		resolve(),
 		commonjs(),
+		json(),
 	],
 }
